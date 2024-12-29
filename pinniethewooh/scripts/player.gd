28042316b0cf0d72eff11c60cce7ca930a8c26fe
abeaduhh <adiba.labonno@gmail.com>
2024-12-28 20:16:48 -0600

    Character can move with specified animations

--- pinniethewooh/scripts/player.gd
+++ pinniethewooh/scripts/player.gd
@@ -5,9 +5,10 @@ extends CharacterBody2D
 @export var starting_direction : Vector2 = Vector2(0,1)
 
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
-	animation_tree.set("parameters/Idle/blend_position", starting_direction)
+	update_params(starting_direction)
 
 func _physics_process(_delta):
 	# Get input directions
@@ -15,7 +16,19 @@ func _physics_process(_delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	
+	update_params(input_direction)
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
+	change_state()
+func update_params(input_direction : Vector2):
+		
+	if(input_direction != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", input_direction)
+		animation_tree.set("parameters/Idle/blend_position", input_direction)
+func change_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
+		
